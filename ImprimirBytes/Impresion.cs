f5f4471d35af1f28370b126ac0185dc31d8bc627
Sahid Roberto Islas Montes <srim3000@hotmail.com>
2015-11-12 18:05:38 -0600

    First Attempt

---
+++ ImprimirBytes/Impresion.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ImprimirBytes
+{
+    class Impresion
+    {
+        private int tipoImpresion;
+        private string textoImpresion;
+
+        public void setTipoImpresion(int tipoImpresion)
+        {
+            this.tipoImpresion = tipoImpresion;
+        }
+
+        public int getTipoImpresion()
+        {
+            return this.tipoImpresion;
+        }
+
+        public void setTextoImpresion(string textoImpresion)
+        {
+            this.textoImpresion = textoImpresion;
+        }
+
+        public string getTextoImpresion()
+        {
+            return this.textoImpresion;
+        }
+
+        public void imprimir()
+        {
+            switch(tipoImpresion)
+            {
+                case 1:
+                    ImpresionByte impresionByte = new ImpresionByte();
+                    impresionByte.setTextoImpresion(textoImpresion);
+                    impresionByte.setTipoImpresion(tipoImpresion);
+                    impresionByte.imprimirBytes();
+                    break;
+                case 2:
+                    ImpresionASCII impresionASCII = new ImpresionASCII();
+                    impresionASCII.setTextoImpresion(textoImpresion);
+                    impresionASCII.setTipoImpresion(tipoImpresion);
+                    impresionASCII.imprimirASCII();
+                    break;
+                case 3:
+                    ImpresionArchivo impresionArchivo = new ImpresionArchivo();
+                    impresionArchivo.setTextoImpresion(textoImpresion);
+                    impresionArchivo.setTipoImpresion(tipoImpresion);
+                    impresionArchivo.imprimirArchivo();
+                    break;
+                default:
+                    Console.Write("Error");
+                    break;
+            }
+        }
+    }
+
+    class ImpresionByte : Impresion
+    {
+        public void imprimirBytes()
+        {
+            Console.WriteLine(convertirTexto(getTextoImpresion()));
+        }
+
+        public string convertirTexto(string ascii)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(ascii);
+
+            foreach (byte b in inputBytes)
+
+            {
+
+                sb.Append(string.Format("{0:x2}", b));
+
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    class ImpresionASCII : Impresion
+    {
+        public void imprimirASCII()
+        {
+            Console.WriteLine(getTextoImpresion());
+        }
+    }
+
+    class ImpresionArchivo : Impresion
+    {
+        StreamWriter nombreArchivo = new StreamWriter("d:\\prueba.txt");
+
+        public void imprimirArchivo()
+        {
+            nombreArchivo.WriteLine(getTextoImpresion());
+            nombreArchivo.Close();
+        }
+    }
+}
---
+++ ImprimirBytes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImprimirBytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Impresion impresion = new Impresion();
+            impresion.setTextoImpresion("Hola a todos");
+            impresion.setTipoImpresion(3);
+            impresion.imprimir();
+        }
+    }
+}
